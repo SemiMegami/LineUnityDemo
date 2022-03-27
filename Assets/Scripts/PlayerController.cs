c9c358a6cfef796c4db02abc538bcba127c3c857
SemiMegami <51983032+SemiMegami@users.noreply.github.com>
2022-03-27 14:50:40 +0700

    First Setting

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float force;
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        rigidbody.AddForce(force * Time.deltaTime * move);
+        Debug.Log(move);
+    }
+}
